24f01ee9bd80388e203fe3662ec928762a95faea
Waldemar Jacek Kuranc <wkuranc@gmail.com>
2016-01-12 17:47:08 +0100

    Initial checkin of source to DaniGWTP course.

---
+++ DaniGWTP/DaniGWTP/src/main/java/com/kuranc/gwtp/DaniGWTP/client/application/ApplicationModule.java
@@ -0,0 +1,14 @@
+package com.kuranc.gwtp.DaniGWTP.client.application;
+
+import com.kuranc.gwtp.DaniGWTP.client.application.home.HomeModule;
+import com.gwtplatform.mvp.client.gin.AbstractPresenterModule;
+
+public class ApplicationModule extends AbstractPresenterModule {
+    @Override
+    protected void configure() {
+        install(new HomeModule());
+
+        bindPresenter(ApplicationPresenter.class, ApplicationPresenter.MyView.class, ApplicationView.class,
+                ApplicationPresenter.MyProxy.class);
+    }
+}
---
+++ DaniGWTP/DaniGWTP/src/main/java/com/kuranc/gwtp/DaniGWTP/client/place/NameTokens.java
@@ -0,0 +1,5 @@
+package com.kuranc.gwtp.DaniGWTP.client.place;
+
+public class NameTokens {
+    public static final String HOME = "/";
+}
